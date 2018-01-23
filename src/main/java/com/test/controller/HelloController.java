4cdeca399d7c69664bf55c6b17d6d448a15d5923
tanghaiting <985663595@qq.com>
2018-01-23 11:55:32 +0800

    idea git 测试

---
+++ src/main/java/com/test/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by D N on 2018/1/19.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(Model model) {
+        return "This is hello!";
+    }
+
+    @RequestMapping("/freeMarker")
+    public String freeMarker(Model model){
+        model.addAttribute("user", "tang");
+        return "index";
+    }
+}
